f7fb3c0deb6ddd5cc3284946b424f42b8e1640ab
Sebastian Mora Villachica <moravillachicasebastian>
2026-07-30 21:07:55 -0600

    adding the exercises from the 4 pillars of OOP

--- Python_Intermedio/4_Pilares_OOP/Ejercicios_4_pilares_OOP.py
+++ Python_Intermedio/4_Pilares_OOP/Ejercicios_4_pilares_OOP.py
@@ -6,7 +6,7 @@
 #Tenga un atributo de min_balance que se pueda asignar al crearla.
 #Arroje un error si al intentar retirar dinero, el retiro haría que el balance quede debajo del min_balance. 
 #Es decir que sí se pueden hacer retiros siempre y cuando el balance quede arriba del min_balance.
-"""
+
 class BankAccount:
 
     def __init__(self, balance):
@@ -54,12 +54,12 @@ account = SavingsAccount(10000, 5000)
 print(account._withdraw_money(2000))
 print(account._withdraw_money(4000))
 print(account._deposit_money(1000))
-"""
+
 #2.Cree una clase abstracta de Shape que:
 #Tenga los métodos abstractos de calculate_perimeter y calculate_area.
 #Ahora cree las siguientes clases que hereden de Shape e implementen esos métodos: Circle, Square y Rectangle.
 #Cada una de estas necesita los atributos respectivos para poder calcular el área y el perímetro.
-"""
+
 from abc import ABC,abstractmethod
 import math
 class Shape(ABC):
@@ -113,7 +113,7 @@ class Rectangle(Shape):
     def calculate_area(self):
         
         return self.width*self.height
-"""
+
     
 #3.Investigue qué usos se le pueden dar a la herencia multiple y cree un ejemplo.
 
